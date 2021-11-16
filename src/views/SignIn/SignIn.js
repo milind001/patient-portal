9eb5c0787760691f621b9a88662b1cfb5f0c6434
Milind Patil <MilindP@citiustech.com>
2021-11-16 11:23:17 +0530

    latestt updated code

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { Button, TextField, Typography, Grid } from "@material-ui/core" 
+import { Link, useHistory } from "react-router-dom";
+import axios from '../../resources/axios';
 import './signin.scss'
-import { Link } from "react-router-dom";
+
 
 const useStyles = makeStyles({
     root: {
@@ -23,38 +25,78 @@ const useStyles = makeStyles({
 
 function SignIn(){
 
-    const classes = useStyles();
+  const [state, setState] = useState({
+      username: "",
+      password: ""
+  });
+  let history = useHistory();
 
-    return(
-      <div className="main-container">
-        <Grid item md={6} xs={12} className="form-container">
-          <form className="inner-form">
-            <Typography className="head-title" variant="h4">Log in to your account</Typography>
-            <TextField
-                variant="outlined"
-                label="Username"
-                placeholder="Enter Username"
-                type="email"
-                className={classes.root}
-            />
-            <TextField
-                variant="outlined"
-                label="Password"
-                placeholder="Enter Password"
-                type="password"
-                className={classes.root}
-            />
-            <div style={{textAlign: 'right'}}><Link to="#" className="forgot-pass">Forgot Password?</Link></div>
+  //onchange input types
+  const handleChange = e => {
+    const {id, value} = e.target;
+    setState(prevState => ({
+        ...prevState,
+        [id]: value
+    }));
+  };
 
-            <Button className="signin-btn" variant="contained" type="submit" color="secondary">login</Button>
+  //submit form
+  const handleSubmit = e => {
+    e.preventDefault();
+        const { username, password } = state;
+        axios.post('/login', {email: username, password: password})
+          .then(res => {
+              console.log(res.data)
+              // localStorage.setItem('token', res.data.accessToken);
+              // localStorage.setItem('email', res.data.user.email);
+              // localStorage.setItem('role', res.data.user.role);
+              history.push('/dashboard');
+          })
+          .catch(err => {
+              console.error(
+                  `Error! in login ${err.message}`
+              );
+          })
+  };
 
-            <center>
-              <div>Don't have an account? <Link to="/signup" style={{color: '#fff'}}>Signup</Link></div>
-            </center>
-          </form>
-        </Grid>
-      </div>
-    )
+  const classes = useStyles();
+
+  return(
+    <div className="main-container">
+      <Grid item md={6} xs={12} className="form-container">
+        <form className="inner-form" onSubmit={handleSubmit}> 
+          <Typography className="head-title" variant="h4">Log in to your account</Typography>
+          <TextField
+              variant="outlined"
+              label="Username"
+              placeholder="Enter Username"
+              type="email"
+              id="username"
+              className={classes.root}
+              value={state.email} 
+              onChange={handleChange}
+          />
+          <TextField
+              variant="outlined"
+              label="Password"
+              placeholder="Enter Password"
+              type="password"
+              id="password"
+              className={classes.root}
+              value={state.password} 
+              onChange={handleChange}
+          />
+          <div style={{textAlign: 'right'}}><Link to="#" className="forgot-pass">Forgot Password?</Link></div>
+
+          <Button className="signin-btn" variant="contained" type="submit" color="secondary">login</Button>
+
+          <center>
+            <div>Don't have an account? <Link to="/signup" style={{color: '#fff'}}>Signup</Link></div>
+          </center>
+        </form>
+      </Grid>
+    </div>
+  )
 };
 
 export default SignIn;
--- src/views/SignUp/Signup.js
+++ src/views/SignUp/Signup.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import { Button, TextField, Typography, Grid } from "@material-ui/core" 
 import { KeyboardDatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers"
 import { Link } from "react-router-dom";
+import axios from '../../resources/axios';
 import './signup.scss'
 
 const useStyles = makeStyles({
@@ -26,12 +27,57 @@ const useStyles = makeStyles({
 function SignUp(){
 
     const classes = useStyles();
-    const [selectedDate, handleDateChange] = useState(new Date());
+    const [dob, setDOB] = useState(new Date());
+
+    const [state, setState] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        mobile: "",
+        password: ""
+    });
+
+    const handleChange = e => {
+        const {id, value} = e.target;
+        setState(prevState => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const { 
+            firstname, 
+            lastname, 
+            email, 
+            mobile,
+            password
+        } = state;
+        const dateDOB = new Date(dob).toISOString();
+        axios.post('/register', {
+            firstname: firstname, 
+            lastname: lastname, 
+            email: email, 
+            mobile: mobile, 
+            password: password, 
+            dob: dateDOB, 
+            role: 1
+        })
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => {
+            console.error(
+                `Error! in login ${err.message}`
+            );
+        })
+     }
 
     return(
         <div className="main-container">
             <Grid item md={6} xs={12} className="form-container">
-                <form className="inner-form">
+                <form className="inner-form" onSubmit={handleSubmit}>
                     <Typography className="head-title" variant="h4">Create an account</Typography>
                     <div className="Username">
                         <TextField
@@ -40,6 +86,8 @@ function SignUp(){
                             placeholder="Enter First Name"
                             type="text"
                             className= {"First_Name " + classes.root}
+                            id="firstname"
+                            onChange={handleChange}
                         />
                         <TextField
                             variant="outlined"
@@ -47,6 +95,8 @@ function SignUp(){
                             placeholder="Enter Last Name"
                             type="text"
                             className={"Last_Name " + classes.root}
+                            id="lastname"
+                            onChange={handleChange}
                         />
                     </div>
                     
@@ -56,19 +106,31 @@ function SignUp(){
                         placeholder="Enter Email"
                         type="email"
                         className={classes.root}
+                        id="email"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="Mobile"
+                        placeholder="Enter Mobile No"
+                        type="text"
+                        className={classes.root}
+                        id="mobile"
+                        onChange={handleChange}
                     />
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                         <Fragment>
                             <KeyboardDatePicker
                                 autoOk
                                 label="DOB"
+                                id="dob"
                                 inputVariant="outlined"
-                                value={selectedDate}
-                                onChange={handleDateChange}
                                 variant="inline"
                                 format="dd/MM/yyyy"
+                                value={dob}
+                                onChange={date => setDOB(date)}
                                 className={classes.root}
-                            />
+                                />
                         </Fragment>
                     </MuiPickersUtilsProvider>
                     
@@ -78,6 +140,8 @@ function SignUp(){
                         placeholder="Enter Password"
                         type="password"
                         className={classes.root}
+                        id="password"
+                        onChange={handleChange}
                     />
                     <TextField
                         variant="outlined"
